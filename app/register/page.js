73913e0dd4a1ba612103bbe674c9a7aa886c831b
GANFON Florentin <114688432+Baguel@users.noreply.github.com>
2024-07-11 18:39:18 +0000

    Update page.js

--- app/register/page.js
+++ app/register/page.js
@@ -8,31 +8,31 @@ export default function Register() {
             <div>
                 <h1 className="text-4xl mt-5 text-center font-bold text-black xs:text-2xl">Créer Votre compte</h1>
             </div>
-            <div className="xs:ml-3 mt-8">
+            <div className="xs:ml-3 xs:mr-3 mt-8">
                 <h1 className="text-2xl font-semibold text-black">Nom Complet</h1>
                 <input className="w-[520px] h-[40px] xs:w-[370px] p-5 mt-2 rounded-lg bg-slate-300" placeholder="Mettez votre nom complet"/>
             </div>
-            <div className="xs:ml-3 mx-auto mt-8">
+            <div className="xs:ml-3 xs:mr-3 mx-auto mt-8">
                 <h1 className="text-2xl font-semibold text-black">Email</h1>
                 <input className="w-[520px] h-[40px] xs:w-[370px] p-5 mt-2 rounded-lg bg-slate-300" placeholder="Mettez votre email"/>
             </div>
-            <div className="xs:ml-3 mx-auto mt-8">
+            <div className="xs:ml-3 xs:mr-3 mx-auto mt-8">
                 <h1 className="text-2xl font-semibold text-black">Numero de Telephone</h1>
                 <input className="w-[520px] h-[40px] xs:w-[370px] p-5 mt-2 rounded-lg bg-slate-300" placeholder="Mettez votre numero de telephone"/>
             </div>
-            <div className="xs:ml-3 mx-auto mt-8">
+            <div className="xs:ml-3 xs:mr-3 mx-auto mt-8">
                 <h1 className="text-2xl font-semibold text-black">Mot de Passe</h1>
                 <input type="password" className="w-[520px] h-[40px] xs:w-[370px] p-5 mt-2 rounded-lg bg-slate-300" placeholder="Mettez votre numero de telephone"/>
             </div>
-            <div className="xs:ml-3 mx-auto mt-8">
+            <div className="xs:ml-3 xs:mr-3 mx-auto mt-8">
                 <h1 className="text-2xl font-semibold text-black">Photo de Profil</h1>
                 <Input className="w-[520px] h-[40px] xs:w-[370px] mt-2 rounded-lg bg-slate-300" id="picture" type="file" />
             </div>
-            <div className="xs:ml-3 mx-auto mt-8">
+            <div className="xs:ml-3 xs:mr-3 mx-auto mt-8">
                 <h1 className="text-2xl font-semibold text-black">Image de Carte</h1>
                 <Input className="w-[520px] h-[40px] xs:w-[370px] mt-2 rounded-lg bg-slate-300" id="background" type="file" />
             </div>
-            <div className="mt-8 flex gap-9 justify-end">
+            <div className="mt-8 xs:mr-3 flex gap-9 justify-end">
                 <button className="bg-slate-400 h-[40px] w-[150px] rounded-md">Se Connecter</button>
                 <button className="bg-green-500 h-[40px] w-[90px] rounded-md">S'incrire</button>
             </div>
