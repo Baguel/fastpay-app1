1e452a911ffc450bb9f20005406c9fb2c2f46def
Baguel <florentin.ganfon@epitech.eu>
2024-07-11 20:19:27 +0000

    creation of frontend

--- app/login/page.js
+++ app/login/page.js
@@ -1,25 +1,25 @@
 import Image from "next/image";
 import { Input } from "@/components/ui/input"
 
-export default function Register() {
+export default function Login() {
   return (
     <main className="h-screen w-full flex items-center justify-center">
-        <div className="">
+        <div className="container mx-auto">
             <div>
                 <Image layout="responsive" width={100} height={100}  src="/undraw_Mobile_pay_re_sjb8 (1).png" alt="payment picture"/>
                 <h1 className="text-4xl mt-5 text-center font-bold text-black xs:text-2xl">Créer Votre compte</h1>
             </div>
-            <div className="xs:ml-3 xs:mr-3 mx-auto mt-8">
+            <div className="mt-8">
                 <h1 className="text-2xl font-semibold text-black">Numero de Telephone</h1>
                 <input className="w-[520px] h-[40px] xs:w-[370px] p-5 mt-2 rounded-lg bg-slate-300" placeholder="Mettez votre numero de telephone"/>
             </div>
-            <div className="xs:ml-3 xs:mr-3 mx-auto mt-8">
+            <div className=" mt-8">
                 <h1 className="text-2xl font-semibold text-black">Mot de Passe</h1>
                 <input type="password" className="w-[520px] h-[40px] xs:w-[370px] p-5 mt-2 rounded-lg bg-slate-300" placeholder="Mettez votre numero de telephone"/>
             </div>
-            <div className="mt-8 xs:mr-3 flex gap-9 justify-end">
+            <div className="mt-8 flex gap-9 justify-end">
                 <button className="bg-slate-400 h-[40px] w-[150px] rounded-md">Se Connecter</button>
-                <button className="bg-green-500 h-[40px] w-[90px] rounded-md">S'incrire</button>
+                <button className="bg-green-500 h-[40px] w-[90px] rounded-md">Sincrire</button>
             </div>
         </div>
     </main>
--- app/register/page.js
+++ app/register/page.js
@@ -3,38 +3,34 @@ import { Input } from "@/components/ui/input"
 
 export default function Register() {
   return (
-    <main className="h-screen w-full flex items-center justify-center">
-        <div className="">
+    <main className="h-screen w-full flex items-center justify-center bg-red-500">
+        <div className="h-[950px] max-w-[700px]">
             <div>
-                <h1 className="text-4xl mt-5 text-center font-bold text-black xs:text-2xl">Créer Votre compte</h1>
+                <h1 className="mt-16 text-center font-bold text-4xl">Créez votre compte</h1>
             </div>
-            <div className="xs:ml-3 xs:mr-3 mt-8">
-                <h1 className="text-2xl font-semibold text-black">Nom Complet</h1>
-                <input className="w-[520px] h-[40px] xs:w-[370px] p-5 mt-2 rounded-lg bg-slate-300" placeholder="Mettez votre nom complet"/>
+            <div className="mt-8">
+                <h1 className="font-semibold text-xl">Nom Complet</h1>
+                <input className="rounded-full lg:w-[580px] h-[40px] xs:w-[400px] sm:w-[400px] md:w-[500px] px-5 font-semibold mt-2" placeholder="Entrez votre nom et prenom"/>
             </div>
-            <div className="xs:ml-3 xs:mr-3 mx-auto mt-8">
-                <h1 className="text-2xl font-semibold text-black">Email</h1>
-                <input className="w-[520px] h-[40px] xs:w-[370px] p-5 mt-2 rounded-lg bg-slate-300" placeholder="Mettez votre email"/>
+            <div className="mt-8">
+                <h1 className="font-semibold text-xl">Email</h1>
+                <input className="rounded-full lg:w-[580px] h-[40px] xs:w-[400px] sm:w-[400px] md:w-[500px] px-5 font-semibold mt-2" placeholder="Entrez votre mail personnel"/>
             </div>
-            <div className="xs:ml-3 xs:mr-3 mx-auto mt-8">
-                <h1 className="text-2xl font-semibold text-black">Numero de Telephone</h1>
-                <input className="w-[520px] h-[40px] xs:w-[370px] p-5 mt-2 rounded-lg bg-slate-300" placeholder="Mettez votre numero de telephone"/>
+            <div className="mt-8">
+                <h1 className="font-semibold text-xl">Numero de Telephone</h1>
+                <input className="rounded-full lg:w-[580px] h-[40px] xs:w-[400px] sm:w-[400px] md:w-[500px] px-5 font-semibold mt-2" placeholder="Entrez votre numero de telephone"/>
             </div>
-            <div className="xs:ml-3 xs:mr-3 mx-auto mt-8">
-                <h1 className="text-2xl font-semibold text-black">Mot de Passe</h1>
-                <input type="password" className="w-[520px] h-[40px] xs:w-[370px] p-5 mt-2 rounded-lg bg-slate-300" placeholder="Mettez votre numero de telephone"/>
+            <div className="mt-8">
+                <h1 className="font-semibold text-xl">Mot de Passe</h1>
+                <input type="password" className="rounded-full lg:w-[580px] h-[40px] xs:w-[400px] sm:w-[400px] md:w-[500px] px-5 font-semibold mt-2" placeholder="Entrez un mot de passe"/>
             </div>
-            <div className="xs:ml-3 xs:mr-3 mx-auto mt-8">
-                <h1 className="text-2xl font-semibold text-black">Photo de Profil</h1>
-                <Input className="w-[520px] h-[40px] xs:w-[370px] mt-2 rounded-lg bg-slate-300" id="picture" type="file" />
+            <div className="mt-8">
+                <h1 className="font-semibold text-xl">Photo de Profil</h1>
+                <Input className="rounded-full lg:w-[580px] h-[40px] xs:w-[400px] sm:w-[400px] md:w-[500px] px-5 font-semibold mt-2" id="picture" type="file" />
             </div>
-            <div className="xs:ml-3 xs:mr-3 mx-auto mt-8">
-                <h1 className="text-2xl font-semibold text-black">Image de Carte</h1>
-                <Input className="w-[520px] h-[40px] xs:w-[370px] mt-2 rounded-lg bg-slate-300" id="background" type="file" />
-            </div>
-            <div className="mt-8 xs:mr-3 flex gap-9 justify-end">
-                <button className="bg-slate-400 h-[40px] w-[150px] rounded-md">Se Connecter</button>
-                <button className="bg-green-500 h-[40px] w-[90px] rounded-md">S'incrire</button>
+            <div className="mt-8">
+                <h1 className="font-semibold text-xl">Image carte FLASHPAY</h1>
+                <Input className="rounded-full lg:w-[580px] h-[40px] xs:w-[400px] sm:w-[400px] md:w-[500px] px-5 font-semibold mt-2" id="picture" type="file" />
             </div>
         </div>
     </main>
