63bdbf02b03b6023a9a2d312b47a3cd0a11bd328
GANFON Florentin <114688432+Baguel@users.noreply.github.com>
2024-07-11 17:36:17 +0000

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="h-screen w-screen bg-green-500">
+    <main className="h-full w-full bg-green-500">
         <div className="h-screen flex gap-2 items-center justify-center">
           <Image width={60} height={60} src="/icons8-money-50.png" alt="money logo"/>
           <h1 className="text-4xl font-bold text-black">FLASHPAY</h1>
